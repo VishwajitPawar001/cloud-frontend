85025b51868c9b52b8ab66793118f10084c3a759
Vishwajit Pawar <pawarvishwajit675@gmail.com>
2026-03-28 00:51:49 +0530

    fixed all api routes

--- services/folders.js
+++ services/folders.js
@@ -1,8 +1,13 @@
 import api from "./api";
 
 export const getFolders = async () => {
-  const res = await api.get("/api/folders");
-  return res.data.folders;
+  try {
+    const res = await api.get("/api/folders");
+    return res.data.folders;
+  } catch (err) {
+    console.error("GET FOLDERS ERROR:", err.response?.data || err.message);
+    return [];
+  }
 };
 
 export const createFolder = async (name) => {
