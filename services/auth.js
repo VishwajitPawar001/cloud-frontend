34d3c2b54f9a16d465141b5c7552b9a386b22ac7
Vishwajit Pawar <pawarvishwajit675@gmail.com>
2026-03-28 00:25:39 +0530

    fixed all api routes

--- services/auth.js
+++ services/auth.js
@@ -1,11 +1,11 @@
 import api from "./api";
 
 export const loginUser = async (email, password) => {
-  const res = await api.post("/auth/login", { email, password });
+  const res = await api.post("/api/auth/login", { email, password });
   return res.data;
 };
 
 export const signupUser = async (email, password) => {
-  const res = await api.post("/auth/signup", { email, password });
+  const res = await api.post("/api/auth/signup", { email, password });
   return res.data;
 };
--- services/folders.js
+++ services/folders.js
@@ -1,14 +1,14 @@
 import api from "./api";
 
 export const getFolders = async () => {
-  const res = await api.get("/folders");
+  const res = await api.get("/api/folders");
   return res.data.folders;
 };
 
 export const createFolder = async (name) => {
-  const res = await api.post("/folders/create", {
+  const res = await api.post("/api/folders/create", {
     name,
-    owner_id: 1
+    owner_id: null
   });
   return res.data;
 };
