376d1d55ad0a9f86c216ec26e575607f534c0c71
Vishwajit Pawar <pawarvishwajit675@gmail.com>
2026-03-28 00:48:45 +0530

    fixed all api routes

--- services/api.js
+++ services/api.js
@@ -4,4 +4,9 @@ const api = axios.create({
   baseURL: "https://cloud-backend-ahwr.onrender.com"
 });
 
+api.interceptors.request.use((config) => {
+  console.log("API Request URL:", config.baseURL + config.url);
+  return config;
+});
+
 export default api;
