159ce5c4b99ded072839feb76d832f4d83b60536
Vishwajit Pawar <pawarvishwajit675@gmail.com>
2026-03-28 00:15:46 +0530

    fixed api base url

--- services/files.js
+++ services/files.js
@@ -1,13 +1,13 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getFiles = async () => {
-  const res = await fetch(`${API_URL}/files`);
+  const res = await fetch(`${API_URL}/api/files`);
   const data = await res.json();
   return data.files;
 };
 
 export const uploadFile = async (formData) => {
-  const res = await fetch(`${API_URL}/files/upload`, {
+  const res = await fetch(`${API_URL}/api/files/upload`, {
     method: "POST",
     body: formData,
   });
@@ -16,19 +16,19 @@ export const uploadFile = async (formData) => {
 };
 
 export const deleteFile = async (id) => {
-  await fetch(`${API_URL}/files/${id}`, {
+  await fetch(`${API_URL}/api/files/${id}`, {
     method: "DELETE",
   });
 };
 
 export const restoreFile = async (id) => {
-  await fetch(`${API_URL}/files/restore/${id}`, {
+  await fetch(`${API_URL}/api/files/restore/${id}`, {
     method: "PUT",
   });
 };
 
 export const permanentDeleteFile = async (id) => {
-  await fetch(`${API_URL}/files/permanent/${id}`, {
+  await fetch(`${API_URL}/api/files/permanent/${id}`, {
     method: "DELETE",
   });
 };
